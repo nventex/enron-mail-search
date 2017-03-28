d339183148bb4e2518af5d21304c099f97ad71b9
Nventex <nventex@gmail.com>
2017-03-27 20:12:11 -0500

    Add component test

--- src/components/search/SearchPage.test.js
+++ src/components/search/SearchPage.test.js
@@ -5,23 +5,26 @@ import { SearchPage } from "./SearchPage";
 import muiTheme from "material-ui/styles/baseThemes/lightBaseTheme";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-import jsdom from "jsdom";
-const doc = jsdom.jsdom("<html><body></body></html>");
-global.document = doc
-global.window = doc.defaultView;
-
 function setup() {
-    const props = {
-        actions: { getInitialState: () => {}, search: (query) => {} },
-        params: {},
-        searchState: { hits: { hits: [] }, query: "", indicatorStatus: "loading", pageNumber: 0 },
-        router: { push: (url) => {}, getCurrentLocation: () => { pathName: "/search" } }
+    const promise = {
+        then: (fn) => { }
     }
     
-    return mount(<MuiThemeProvider><SearchPage {...props}/></MuiThemeProvider>);
+    const props = {
+        actions: { getInitialState: () => {}, search: (query) => { return Promise.resolve() } },
+        params: {},
+        searchState: { hits: { hits: [] }, query: "", indicatorStatus: "loading", pageNumber: 0 },
+        router: { push: (url) => {}, getCurrentLocation: () => { return { pathName: "/search" } } }
+    }
+    
+    return mount(<MuiThemeProvider><SearchPage {...props}/></MuiThemeProvider>, 
+        { context: { router: { push: (url) => { } } } });
 }
 
 test("Returns true when url contains the search term and page number", (t) => {
-    var wrapper = setup();
+    const wrapper = setup();
+    const searchButton = wrapper.find("button");
+    searchButton.simulate("click");
+    debugger;
     t.end();
 });
