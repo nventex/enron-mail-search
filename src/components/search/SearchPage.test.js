a6f3895d974421de8b412b3edbd5cac77f050b24
Nventex <nventex@gmail.com>
2017-04-09 16:01:38 -0500

    added unit tests for SearchPage

--- src/components/search/SearchPage.test.js
+++ src/components/search/SearchPage.test.js
@@ -1,30 +1,43 @@
 import React from "react";
 import test from "tape";
-import { mount, shallow } from "enzyme";
+import { shallow } from "enzyme";
 import { SearchPage } from "./SearchPage";
-import muiTheme from "material-ui/styles/baseThemes/lightBaseTheme";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-function setup() {
-    const promise = {
-        then: (fn) => { }
-    }
-    
+function setup(data) {
     const props = {
         actions: { getInitialState: () => {}, search: (query) => { return Promise.resolve() } },
         params: {},
         searchState: { hits: { hits: [] }, query: "", indicatorStatus: "loading", pageNumber: 0 },
-        router: { push: (url) => {}, getCurrentLocation: () => { return { pathName: "/search" } } }
+        router: { push: (url) => {}, getCurrentLocation: () => { return { pathname: data.pathname } } }
     }
     
-    return mount(<MuiThemeProvider><SearchPage {...props}/></MuiThemeProvider>, 
-        { context: { router: { push: (url) => { } } } });
+    return shallow(<SearchPage {...props}/>);
 }
 
-test("Returns true when url contains the search term and page number", (t) => {
-    const wrapper = setup();
-    const searchButton = wrapper.find("button");
-    searchButton.simulate("click");
-    debugger;
+test("Returns true when the uri requested contains search, the term and page number", (t) => {
+    const wrapper = setup({ pathname: "/search/term/1" });
+    let result = wrapper.instance().hasUriParams();
+    t.true(result);
+    t.end();
+});
+
+test("Returns false when the uri requested does not contain search", (t) => {
+    const wrapper = setup({ pathname: "/path/term/1" });
+    let result = wrapper.instance().hasUriParams();
+    t.false(result);
+    t.end();
+});
+
+test("Returns false when the uri requested does not contain the term", (t) => {
+    const wrapper = setup({ pathname: "/search/1" });
+    let result = wrapper.instance().hasUriParams();
+    t.false(result);
+    t.end();
+});
+
+test("Returns false when the uri requested does not contain the page number", (t) => {
+    const wrapper = setup({ pathname: "/search/term" });
+    let result = wrapper.instance().hasUriParams();
+    t.false(result);
     t.end();
 });
